504b110e144bcacb7493451f23117c04692b0c79
Jeffrey Ladd <jladd@Jeffreyladd.net>
2023-07-14 09:33:41 -0400

    Fixes MAUI App. Adds WPF App. Fixes various bogus warnings

---
+++ src/TPIE.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+using TPIE.Data;
+
+namespace TPIE.Wpf;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddWpfBlazorWebView();
+        serviceCollection.AddBlazorWebViewDeveloperTools();
+        serviceCollection.AddSingleton<WeatherForecastService>();
+        Resources.Add("services", serviceCollection.BuildServiceProvider());
+    }
+}
